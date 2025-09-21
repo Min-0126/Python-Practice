8deb0f40aca8bdf38eb963cb3d41f046297b1315
Min-0126 <minha0126@proton.me>
2025-09-22 01:06:02 +0900

    Create project_hello.py

---
+++ Chapter1/project_hello.py
@@ -0,0 +1,8 @@
+print('Hellow, world!')
+print('What is your name?') #Ask your name
+my_name = input ('>')
+print('It is good to meet you,'+ my_name)
+print(len(my_name))
+print('What is your age?') #Ask for age
+my_age = input('>')
+print('You will be ' + str(int(my_age)) + ' in a year.')
