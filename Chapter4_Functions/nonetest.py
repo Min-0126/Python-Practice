8606809dcc6888bc62f0814b603846a4ab14918c
Min-0126 <minha0126@proton.me>
2025-10-05 00:00:58 +0900

    Create nonetest.py

---
+++ Chapter4_Functions/nonetest.py
@@ -0,0 +1,7 @@
+# Print doesn't have return value like len() and input()
+# However, all function call needs to evaluate to a return value
+# Therefore, print() function returns None
+
+spam = print('Hello')
+
+None == spam
