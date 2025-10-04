1137d95530028a08074f2e15b4668d7937621dac
Min-0126 <minha0126@proton.me>
2025-10-04 14:09:14 +0900

    Create fiveTimes.py

---
+++ Chapter3_Loop/fiveTimes.py
@@ -0,0 +1,27 @@
+print('Hello!')
+
+# i from 0 to 5 but not include 5
+for i in range(5):
+    print('Iteration i is set to ' + str(i))
+
+#Change for loop into while loop. Same result
+print("Change For loop into While loop")
+print('Hello!')
+i = 0
+while i<5:
+    print('Iteration i is set to ' + str(i))
+    i = i + 1
+print('Goodbye!')
+
+#For loop in range
+
+#i from 0 to 10 with interval 2
+for i in range (0, 10, 2):
+    print(i)
+
+#i from 5 to -1 with interval -1
+for i in range (5, -1, -1):
+    print(i)
+
+
+
