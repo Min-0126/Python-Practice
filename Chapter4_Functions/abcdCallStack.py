710a7a91bd50e8bd2be5cbbb391d3649d37341c7
Min-0126 <minha0126@proton.me>
2025-10-05 00:43:12 +0900

    Create abcdCallStack.py

---
+++ Chapter4_Functions/abcdCallStack.py
@@ -0,0 +1,23 @@
+def a():
+    print('a() starts')
+    b()
+    d()
+    print('a() returns')
+
+def b():
+    print('b() starts')
+    c()
+    print('b() returns')
+
+def c():
+    print('c() starts')
+    print('c() returns')
+
+def d():
+    print('d() starts')
+    print('d() returns')
+
+a()
+
+# As -> Bs -> Cs -> Cr -> Br -> Ds -> Dr -> Ar
+# This is stack
