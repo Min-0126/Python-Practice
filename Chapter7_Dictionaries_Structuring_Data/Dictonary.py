325ac3c1cdce97c6b01729f44c3fd07a5012ccad
Min-0126 <minha0126@proton.me>
2025-10-10 00:59:38 +0900

    Create Dictonary.py

---
+++ Chapter7_Dictionaries_Structuring_Data/Dictonary.py
@@ -0,0 +1,11 @@
+# Dictonary is a mutable collection of many values
+# Can use many different data types
+# Compose with 'key' and 'value'
+# Use key to find value     (Mapping)
+
+my_cat = {'size' : 'fat', 'color': 'gray', 'age':17}
+# my_cat['size']     -> 'fat'
+
+
+# Dictonaires are unordered. It is just group of variables
+# Therefore, the order of dictionary key-value pairs doesn't matter
