4825057a2728504d3f5b701859949a5ffcb23401
Min-0126 <minha0126@proton.me>
2025-10-09 15:01:27 +0900

    Create index.py

---
+++ Chapter6_List/index.py
@@ -0,0 +1,19 @@
+# Index
+# spam[0] -> Cat, spam[1] -> Bat, spam[2] -> Rat, spam[3] -> Elephant
+
+# Negative Index
+# spam[-1] -> Elephant, spam[-3] -> Bat
+
+# Slices
+# spam[0:4] -> ["Cat" , "Bat", "Rat", "Elephant"]
+# spam[1:3] -> ['Bat', 'Rat']
+# spam[0:-1] -> ['Cat', 'Bat', 'Rat']
+# spam[1:] -> ["Bat", "Rat", "Elephant"]
+
+# len function
+# len(spam) -> 4
+spam = ["Cat" , "Bat", "Rat", "Elephant"]
+
+# value update
+# spamp[1] = 'aardvark'
+# spam = ["Cat" , "aardvark", "Rat", "Elephant"]
